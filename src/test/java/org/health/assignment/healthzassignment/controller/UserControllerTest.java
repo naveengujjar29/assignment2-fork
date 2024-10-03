e12a6180888b3b5b9e0c5ac5951d7eaafd3bb3f9
Naveen Kumar <naveengujjar29@gmail.com>
2024-10-03 20:59:57 +0530

    correcting message

--- src/test/java/org/health/assignment/healthzassignment/controller/UserControllerTest.java
+++ src/test/java/org/health/assignment/healthzassignment/controller/UserControllerTest.java
@@ -7,10 +7,12 @@ import org.health.assignment.healthzassignment.utils.CommonUtils;
 import org.health.assignment.healthzassignment.utils.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -20,6 +22,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 
--- src/test/java/org/health/assignment/healthzassignment/service/UserServiceTest.java
+++ src/test/java/org/health/assignment/healthzassignment/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
